8469f8b11a38130f90cb34093164a58adc75ea66
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-11-10 19:26:43 -0300

    feat: framer motion adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 // App.tsx
 import { Header } from "./components/header/Header";
 import { useDarkMode } from "./components/theme/useDarkMode";
+import { motion } from "framer-motion";
 
 function App() {
   const { dark, toggleDark } = useDarkMode();
 
   return (
-    <div
-      className={`min-h-screen transition-colors duration-300 ${
-        dark ? "dark bg-gray-900 text-white" : "bg-white text-black"
-      }`}
+    <motion.div
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      className="min-h-screen bg-white dark:bg-gray-900"
     >
-      <Header dark={dark} onToggle={toggleDark} />
-    </div>
+      <div
+        className={`min-h-screen transition-colors duration-300 ${
+          dark ? "dark bg-gray-900 text-white" : "bg-white text-black"
+        }`}
+      >
+        <Header dark={dark} onToggle={toggleDark} />
+      </div>
+    </motion.div>
   );
 }
 
--- src/data/contactIconsHeader.ts
+++ src/data/contactIconsHeader.ts
@@ -1,5 +1,5 @@
-import InstagramIcon from "../../assets/icons/instagramIcon.svg";
-import XIcon from "../../assets/icons/xIcon.svg";
-import EmailIcon from "../../assets/icons/emailIcon.svg";
+import InstagramIcon from "../assets/icons/instagramIcon.svg";
+import XIcon from "../assets/icons/xIcon.svg";
+import EmailIcon from "../assets/icons/emailIcon.svg";
 
 export const contactIcons = [InstagramIcon, XIcon, EmailIcon];
